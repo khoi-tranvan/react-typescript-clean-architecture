b2c971bae0b7c9d1ecc22d2051083dbf308c1f6b
thang.buingoc <thang.buingoc@vti.com.vn>
2023-07-06 11:46:43 +0700

    add config

--- src/infrastructure/api/Auth.ts
+++ src/infrastructure/api/Auth.ts
@@ -1,16 +1,16 @@
 import { SignInData, SignUpData } from "../../domain/entities/Auth";
 import { AuthRepository } from "../../application/repositories/AuthRepository";
-import { axiosConfig } from "../config";
-
+import configApi from "../config/axios";
 export class AuthAPI implements AuthRepository {
-  async signin(data: SignInData): Promise<any> {
+  async signin(data: SignInData, controller: AbortController): Promise<any> {
     const API_URL = "/sign-in";
-    const response = await axiosConfig.post(API_URL, data);
+    const CONFIG = configApi(controller)
+    const response = await CONFIG.axiosConfig.post(API_URL, data);
     return response.data;
   }
-  async signup(data: SignUpData): Promise<any> {
-    const API_URL = "/sign-up";
-    const response = await axiosConfig.post(API_URL, data);
-    return response.data;
-  }
+//   async signup(data: SignUpData): Promise<any> {
+//     const API_URL = "/sign-up";
+//     const response = await axiosConfig.post(API_URL, data);
+//     return response.data;
+//   }
 }
--- src/infrastructure/config/axios.ts
+++ src/infrastructure/config/axios.ts
@@ -5,72 +5,83 @@ import { UserStorage } from "../storage/UserStorage";
 // https://lightrains.com/blogs/axios-intercepetors-react/
 
 // control api call
-const controller = new AbortController();
-const CancelToken = axios.CancelToken;
-const source = CancelToken.source();
-const userStorage = new UserStorage();
 
-const axiosConfig = axios.create({
-  baseURL: process.env.REACT_APP_API_END_POINT,
-  timeout: ENV.TIMEOUT,
-  cancelToken: source.token,
-  signal: controller.signal,
-  paramsSerializer: (params) => queryString.stringify(params),
-});
+const configApi = (controller: AbortController) => {
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+    const userStorage = new UserStorage();
 
-axiosConfig.interceptors.request.use(
-  (config) => {
-    const token = userStorage.getAccessToken();
-    if (token && config.headers) {
-      config.headers["Authorization"] = "Bearer " + token;
-    }
-    if (config.headers) {
-      config.headers["Content-Type"] = "application/json";
-    }
-    return config;
-  },
-  (error) => {
-    Promise.reject(error);
-  }
-);
+    const axiosConfig = axios.create({
+        baseURL: process.env.REACT_APP_API_END_POINT,
+        timeout: ENV.TIMEOUT,
+        cancelToken: source.token,
+        paramsSerializer: (params) => queryString.stringify(params),
+    });
 
-axiosConfig.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (!window.navigator.onLine) {
-      window.location.reload();
-      return;
-    }
-    if (error?.response?.status === 401) {
-      controller.abort(); // stop other when
-      try {
-        const response = await axios.post(
-          "/auth/refresh",
-          {
-            refreshToken: userStorage.getRefreshToken(),
-          },
-          {
-            headers: {
-              Authorization: "Bearer " + userStorage.getAccessToken(), //the token is a variable which holds the token
-            },
-            baseURL: process.env.REACT_APP_API_END_POINT,
-            timeout: ENV.TIMEOUT,
-          }
-        );
-        localStorage.removeItem("user");
-        localStorage.setItem("user", JSON.stringify(response.data.results));
-        window.location.reload();
-      } catch (error) {
-        window.localStorage.removeItem("user");
-        window.location.replace("/");
-        return Promise.reject(error);
-      }
-    } else {
-      throw error?.response?.data ? error.response.data : error.response;
-    }
-  }
-);
+    axiosConfig.interceptors.request.use(
+        (config) => {
+            const token = userStorage.getAccessToken();
+            if (token && config.headers) {
+                config.headers["Authorization"] = "Bearer " + token;
+            }
+            if (config.headers) {
+                config.headers["Content-Type"] = "application/json";
+            }
+            return config;
+        },
+        (error) => {
+            Promise.reject(error);
+        }
+    );
 
-export default axiosConfig;
+    axiosConfig.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        async (error) => {
+            if (!window.navigator.onLine) {
+                window.location.reload();
+                return;
+            }
+            if (error?.response?.status === 401) {
+                controller.abort(); // stop other when
+                try {
+                    const response = await axios.post(
+                        "/auth/refresh",
+                        {
+                            refreshToken: userStorage.getRefreshToken(),
+                        },
+                        {
+                            headers: {
+                                Authorization:
+                                    "Bearer " + userStorage.getAccessToken(), //the token is a variable which holds the token
+                            },
+                            baseURL: process.env.REACT_APP_API_END_POINT,
+                            timeout: ENV.TIMEOUT,
+                        }
+                    );
+                    localStorage.removeItem("user");
+                    localStorage.setItem(
+                        "user",
+                        JSON.stringify(response.data.results)
+                    );
+                    window.location.reload();
+                } catch (error) {
+                    window.localStorage.removeItem("user");
+                    window.location.replace("/");
+                    return Promise.reject(error);
+                }
+            } else {
+                throw error?.response?.data
+                    ? error.response.data
+                    : error.response;
+            }
+        }
+    );
+
+    return {
+        axiosConfig,
+    };
+};
+
+export default configApi;
